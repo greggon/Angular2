7727c5d9a0c03a59062aefdfc7be8fdf5ce1f980
greggon <gregory.goncharov@gmail.com>
2017-02-06 12:48:39 -0500

    Adding component style for thead

--- APM/app/products/product-list.component.ts
+++ APM/app/products/product-list.component.ts
@@ -6,6 +6,7 @@ import { IProduct } from './product';
 @Component({
     selector: 'pm-products',
     templateUrl: 'app/products/product-list.component.html'
+    styleUrls: ['app/products/product-list.component.css']
 })
 
 export class ProductListComponent {
