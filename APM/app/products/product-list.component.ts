895c77b5f71f3f73ded33457e2a68747c5faac43
greggon <gregory.goncharov@gmail.com>
2017-02-06 12:44:31 -0500

    adding Product interface

--- APM/app/products/product-list.component.ts
+++ APM/app/products/product-list.component.ts
@@ -1,5 +1,7 @@
 import { Component} from '@angular/core';
 
+import { IProduct } from './product';
+
 
 @Component({
     selector: 'pm-products',
@@ -12,7 +14,7 @@ export class ProductListComponent {
     imageMargin: number=2;
     showImage: boolean=false;
     listFilter: string = 'cart';
-    products: any[]= [
+    products: IProduct[]= [
      {
         "productId": 1,
         "productName": "Leaf Rake",
